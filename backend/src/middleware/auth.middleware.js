4372a05fe20add11fb49083378a60698cd1da04f
ADITYA_DR <raybandarkaraditya@gmail.com>
2025-11-10 22:41:20 +0530

    try

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/User.models.js";
+import User from "../models/user.models.js";
 
 export const protectRoute = async(req,res,next)=>{
     try {
